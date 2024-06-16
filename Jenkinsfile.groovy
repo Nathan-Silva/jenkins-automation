b1041b71e4b1ceac18c6155f705cc0ba900c7717
nathanbraga <braganathan38@gmail.com>
2024-06-15 23:28:15 -0300

    ci: jenkins file created

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,5 +7,17 @@ pipeline {
                 echo 'Realizando o Build da aolicacao'
             }
         }
+
+        stage('Tests') {
+            echo 'Realizando os testes'
+        }
+
+        stage('Code Scanner') {
+            echo 'Escaneando o codigo'
+        }
+
+        stage('Deploy Kubernetes') {
+            echo 'Realizando o Deploy em prod K8s'
+        }
     }
 }
